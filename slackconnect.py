c50709cc7de268ba0ee787737c4a705a161c433e
Ciaran Trevino <ciaranrtrevino@gmail.com>
2024-03-13 23:01:27 -0700

    changed urls env endpoints to heroku app

--- slackconnect.py
+++ slackconnect.py
@@ -30,11 +30,11 @@ app.logger.setLevel(logger.level)
 
 
 # Assuming the environment variables are set for the URLs of the oauthoption.py endpoints
-OAUTH_JIRA_URL = os.getenv("OAUTH_JIRA_URL", "http://localhost:3000/start-oauth-jira")
-OAUTH_SHEETS_URL = os.getenv("OAUTH_SHEETS_URL", "http://localhost:3000/start-oauth-sheets")
-PROCESS_MESSAGE_URL = os.getenv("PROCESS_MESSAGE_URL", "http://localhost:3000/process-message")
-START_ASSISTANT_URL = os.getenv("START_ASSISTANT_URL", "http://localhost:3000/start-assistant")
-GET_LATEST_MESSAGES_URL = os.getenv("GET_LATEST_MESSAGES_URL", "http://localhost:3000/get-latest-messages")
+OAUTH_JIRA_URL = os.getenv("OAUTH_JIRA_URL", "https://jiraslackgpt-592ed3dfdc03.herokuapp.com/start-oauth-jira")
+OAUTH_SHEETS_URL = os.getenv("OAUTH_SHEETS_URL", "https://jiraslackgpt-592ed3dfdc03.herokuapp.com/start-oauth-sheets")
+PROCESS_MESSAGE_URL = os.getenv("PROCESS_MESSAGE_URL", "https://jiraslackgpt-592ed3dfdc03.herokuapp.com/process-message")
+START_ASSISTANT_URL = os.getenv("START_ASSISTANT_URL", "https://jiraslackgpt-592ed3dfdc03.herokuapp.com/start-assistant")
+GET_LATEST_MESSAGES_URL = "https://jiraslackgpt-592ed3dfdc03.herokuapp.com/get-latest-messages"
 
 def verify_slack_request(request):
     request_body = request.get_data().decode('utf-8')
